cfd53ab710194bff2c78dcb2d8fe175286486f86
baso034 <55867963+baso034@users.noreply.github.com>
2020-06-16 20:59:37 +0530

    update for on files

--- PDF_merger.py
+++ PDF_merger.py
@@ -1,12 +1,15 @@
 import os
 from PyPDF2 import PdfFileMerger
+import sys
 
 all_files = os.listdir()
 pdfs = []
 for each_file in all_files:
     if each_file.endswith(".pdf")>0:
         pdfs.append(each_file)
-
+if len(pdfs)==0:
+    sys.exit()
+    
 merger = PdfFileMerger()
 
 for pdf in pdfs:
